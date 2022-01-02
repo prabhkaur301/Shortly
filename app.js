cb225d7190aa15d3a199929dc9856f14252e1a51
prabhkaur301 <you@example.com>
2022-01-02 23:57:45 +0530

    Javascript done

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const shortUrl=document.querySelector('#shortLink');
 const invalid=document.querySelector('.valid-link');
 const shortBtn=document.querySelector('.shorten');
 shortBtn.addEventListener('click',()=>{
-let query=shortUrl.value;
+const query=shortUrl.value;
 console.log(query)
 if(query===""){
 shortUrl.classList.toggle('invalid-input');
@@ -27,7 +27,8 @@ else{
     invalid.classList.remove('invalid');
     shortUrl.classList.remove('invalid-input');
     fetchApi(query);
-    showResults();
+    
+    shortUrl.value='';
 }
 });
 
@@ -38,6 +39,35 @@ const data= await response.json();
 console.log(data);
 const finalLink=data.result.short_link;
 console.log(finalLink);
+showResults(finalLink,query);
 }
 
+const showResults= (finalLink,query)=>{
+    
+const shortedLinks=document.querySelector('.shorted-links');
+const linkBlock=document.createElement("div");
+linkBlock.classList.add("short-block");
+const queryText=document.createElement("div");
+queryText.classList.add("query");
+queryText.innerHTML=query;
+const link=document.createElement("div");
+link.classList.add("link");
+link.innerHTML=finalLink;
+const copyButton=document.createElement("button");
+copyButton.classList.add("copy");
+copyButton.classList.add("btn");
+copyButton.innerHTML="Copy";
+linkBlock.appendChild(queryText);
+linkBlock.appendChild(link);
+linkBlock.appendChild(copyButton);
+shortedLinks.appendChild(linkBlock);
+copyButton.addEventListener('click',()=>{
+    let copied = link.textContent;
 
+          navigator.clipboard.writeText(copied).then(() => {
+            copyButton.textContent = 'Copied!';
+            copyButton.style.background = 'hsl(257, 27%, 26%)';
+          });
+});
+
+};
